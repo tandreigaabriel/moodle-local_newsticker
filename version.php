365b0dc34f6b3153c386ef4f243921e5a89af27e
Toma Andrei G <tandreigaabriel@gmail.com>
2026-06-20 18:00:33 +0100

    Remove donation text from README support section

--- version.php
+++ version.php
@@ -8,11 +8,11 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// along with Moodle. If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * Plugin version information.
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_newsticker';
-$plugin->version = 2026052101;
+$plugin->version = 2026062000;
 $plugin->requires = 2024100701; // Moodle 4.5.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v1.0.2';
+$plugin->release = 'v1.0.3';
